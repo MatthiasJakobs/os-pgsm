db651f008d5a6a90b97d85d944f7ca7189d0dd8a
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2021-02-25 21:36:42 +0100

    first (working?) version

---
+++ jena_climate.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import torch
+
+# based on https://www.tensorflow.org/tutorials/structured_data/time_series
+class Jena_Climate:
+
+    # TODO: If we use other columns: needs more cleanup! See link above
+    def __init__(self, path="code/jena_climate_2009_2016.csv", cols=['T (degC)'], normalize=True):
+        self.df = pd.read_csv(path)[5::6]
+        self.df = self.df[cols]
+
+        self.means = self.df.mean()
+        self.stds = self.df.std()
+
+        if normalize:
+            self.df = (self.df - self.means) / self.stds
+
+    def torch(self):
+        pth = torch.from_numpy(self.df.to_numpy()).squeeze().float()
+        return pth
--- main.py
+++ main.py
@@ -6,17 +6,14 @@ from tsx.attribution import Grad_CAM
 from tsx.models.forecaster import Shallow_FCN, Shallow_CNN_RNN
 from tsx.datasets.ucr import load_itapowdem
 from tsx.visualizations import plot_cam
-from tsx.metrics import smape
+from tsx.metrics import smape, mae
 from tsx.utils import to_numpy
 import matplotlib.pyplot as plt 
-
-# TODO: Preprocessing
-# TODO: Support preprocessing from sklearn as paramters
-ds = load_itapowdem()
-A, _ = ds.torch(train=True)
-B, _ = ds.torch(train=False)
-
-X = torch.cat((A, B), dim=0)
+from wheatprice import WheatPrice
+from jena_climate import Jena_Climate
+from ranker import Ranker
+from tsx.distances import dtw
+from tqdm import tqdm
 
 def split_timeseries_percent(X, percentages):
     total_length = len(X)
@@ -29,40 +26,67 @@ def split_timeseries_percent(X, percentages):
 
     return splits
 
-def compare_logs(log_a, log_b, mse_baseline=None, smape_baseline=None):
-    plt.figure()
+def windowing(X, input_width=3, offset=0, label_width=1, split_percentages=(0.7, 0.2, 0.1)):
+    nr_output_samples = int(np.floor(len(X) / (input_width + label_width + offset)))
 
-    plt.subplot(1, 3, 1)
-    plt.title("train_loss")
-    plt.plot(log_a.index, log_a['train_loss'], color="blue", label="CNN+LSTM")
-    plt.plot(log_b.index, log_b['train_loss'], color="green", label="CNN")
-    plt.legend()
+    x_w = []
+    y_w = []
 
-    plt.subplot(1, 3, 2)
-    plt.title("val_mse")
-    plt.plot(log_b.index, log_b['val_loss'], color="blue", label="CNN+LSTM")
-    plt.plot(log_a.index, log_a['val_loss'], color="green", label="CNN")
-    plt.hlines(mse_bseline, 0, log_a.index[-1], colors="red", label="Baseline")
-    plt.legend()
+    for i in range(0, len(X), (input_width+offset+label_width)):
+        x_w.append(np.expand_dims(X[i:(i+input_width)], 0))
+        y_w.append(np.expand_dims(X[(i+input_width+offset):(i+input_width+offset+label_width)], 0))
 
-    plt.subplot(1, 3, 3)
-    plt.title("val_smape")
-    plt.plot(log_b.index, log_b['val_smape'], color="blue", label="CNN+LSTM")
-    plt.plot(log_a.index, log_a['val_smape'], color="green", label="CNN")
-    plt.hlines(smape_baseline, 0, log_a.index[-1], colors="red", label="Baseline")
-    plt.legend()
+    if x_w[-1].shape != x_w[0].shape:
+        x_w = x_w[:-1]
+        y_w = y_w[:-1]
 
-    plt.show()
+    if y_w[-1].shape[-1] == 0:
+        x_w = x_w[:-1]
+        y_w = y_w[:-1]
 
-def rank_models(pred_a, pred_b, x, y):
+    x_w = np.concatenate(x_w, axis=0)
+    y_w = np.concatenate(y_w, axis=0)
+
+    return split_timeseries_percent(x_w, split_percentages), split_timeseries_percent(y_w, split_percentages)
+
+def compare_logs(log_a, log_b, mse_baseline=None, mae_baseline=None, smape_baseline=None):
+
+    _, axes = plt.subplots(2, 2, figsize=(10, 6))
+    axes.flat[0].set_title("train_mse")
+    axes.flat[0].plot(log_a.index, log_a['train_mse'], color="blue", label="CNN+LSTM")
+    axes.flat[0].plot(log_b.index, log_b['train_mse'], color="green", label="CNN")
+    axes.flat[0].legend()
+
+    axes.flat[1].set_title("val_mae")
+    axes.flat[1].plot(log_b.index, log_b['val_mae'], color="blue", label="CNN+LSTM")
+    axes.flat[1].plot(log_a.index, log_a['val_mae'], color="green", label="CNN")
+    axes.flat[1].hlines(mae_baseline, 0, log_a.index[-1], colors="red", label="Baseline")
+    axes.flat[1].legend()
+
+    axes.flat[2].set_title("val_smape")
+    axes.flat[2].plot(log_b.index, log_b['val_smape'], color="blue", label="cnn+lstm")
+    axes.flat[2].plot(log_a.index, log_a['val_smape'], color="green", label="cnn")
+    axes.flat[2].hlines(smape_baseline, 0, log_a.index[-1], colors="red", label="baseline")
+    axes.flat[2].legend()
+
+    axes.flat[3].set_title("val_mse")
+    axes.flat[3].plot(log_b.index, log_b['val_mse'], color="blue", label="cnn+lstm")
+    axes.flat[3].plot(log_a.index, log_a['val_mse'], color="green", label="cnn")
+    axes.flat[3].hlines(mse_baseline, 0, log_a.index[-1], colors="red", label="baseline")
+    axes.flat[3].legend()
+
+    plt.tight_layout()
+    plt.savefig("../plots/poc_train_logs.pdf")
+
+def rank_models(model_predictions, y, dist=smape):
     # TODO: Assumes two models for now, make this more general
     y = to_numpy(y)
+    errors = np.zeros((len(model_predictions), len(y)))
 
-    rel_err_a = np.abs(np.squeeze(y) - np.squeeze(pred_a))
-    rel_err_b = np.abs(np.squeeze(y) - np.squeeze(pred_b))
-
-    ranking = [1 if err_a <= err_b else 0 for (err_a, err_b) in zip(rel_err_a, rel_err_b)]
-    return np.array(ranking)
+    for i, pred in enumerate(model_predictions):
+        errors[i] = dist(y, pred.reshape(-1, 1), axis=1)
+    
+    return np.argsort(-errors, axis=0)
 
 def get_gradcam(models, preds, x, y):
     cams = []
@@ -72,22 +96,78 @@ def get_gradcam(models, preds, x, y):
 
     return cams
 
-intervals_percent = [.7, .2, .1]
-x_train, x_val, x_test = split_timeseries_percent(X, intervals_percent)
+# TODO: This is normalized, i.e., think about this threshold more?
+# Idea: Smooth out region of competence
+#       - If a single point is surrounded by points below threshold, it will be deleted aswell (denoising)
+def get_rocs(rankings, predictions, models, validation_data, validation_label):
 
-y_train = x_train[..., -1]
-y_val = x_val[..., -1]
-y_test = x_test[..., -1]
+    rocs = []
 
-x_train = x_train[..., :-1]
-x_val = x_val[..., :-1]
-x_test = x_test[..., :-1]
+    for i, m in enumerate(models):
 
-epochs = 5
+        # region of competence shape: (nr_of_best, length_of_explanation)
+        is_best = rankings[i]
+        model_roc = np.zeros((np.sum(is_best), 3)) # TODO: This needs to be more generic
 
+        for x_i in range(model_roc.shape[0]):
+            m.reset_gradients() 
+            out = m.forward(validation_data[np.where(is_best)][x_i].unsqueeze(0), return_intermediate=True)
+            feats = out['feats']
+            logits = out['logits']
+
+            grads = torch.autograd.grad(smape(logits, validation_label[np.where(is_best)][x_i]), feats)[0].squeeze()
+
+            a = grads.detach()
+            A = feats.detach()
+
+            r = torch.sum(torch.nn.functional.relu(a * A), axis=1).numpy()
+            model_roc[x_i] = r / np.max(r)
+
+        rocs.append(model_roc)
+
+    return rocs
+
+def online_forecasting(rocs, models, X, dist=dtw):
+    predictions = []
+
+    for x in tqdm(X):
+
+        best_model = -1
+        best_indice = None
+        smallest_distance = 1e8
+
+        for i, m in enumerate(models):
+
+            cam = Grad_CAM(x.unsqueeze(0), None, m).numpy()
+            for r in rocs[i]:
+                distance = dist(r, cam)
+                if distance < smallest_distance:
+                    best_model = i
+                    smallest_distance = distance
+
+        predictions.append(models[best_model].predict(x.unsqueeze(0)))
+
+    return np.array(predictions)
+    
+
+
+
+# TODO: Support preprocessing from sklearn as paramters
+X = Jena_Climate().torch()
+
+[x_train, x_val, x_test], [y_train, y_val, y_test] = windowing(X, input_width=3)
+x_train = torch.from_numpy(x_train).unsqueeze(1).float()
+x_val = torch.from_numpy(x_val).unsqueeze(1).float()
+x_test = torch.from_numpy(x_test).unsqueeze(1).float()
+y_train = torch.from_numpy(y_train).float()
+y_val = torch.from_numpy(y_val).float()
+y_test = torch.from_numpy(y_test).float()
+
+epochs = 70
+batch_size = 300
 # TODO: Store model
-model_a = Shallow_CNN_RNN(batch_size=20, epochs=epochs, hidden_states=30)
-model_b = Shallow_FCN(batch_size=20, epochs=epochs)
+model_a = Shallow_CNN_RNN(batch_size=batch_size, epochs=epochs, transformed_ts_length=x_train.shape[-1], hidden_states=100)
+model_b = Shallow_FCN(batch_size=batch_size, ts_length=x_train.shape[-1], epochs=epochs)
 
 logs_a = model_a.fit(x_train, y_train, X_val=x_val, y_val=y_val)
 logs_b = model_b.fit(x_train, y_train, X_val=x_val, y_val=y_val)
@@ -95,17 +175,26 @@ logs_b = model_b.fit(x_train, y_train, X_val=x_val, y_val=y_val)
 preds_a = model_a.predict(x_val)
 preds_b = model_b.predict(x_val)
 
-baseline_prediction = x_val[..., -1]
-smape_baseline = smape(baseline_prediction, y_val)
-mse_bseline = torch.nn.MSELoss()(baseline_prediction, y_val)
+rankings = rank_models([preds_a, preds_b], y_val)
+
+val_rocs = get_rocs(rankings, [preds_a, preds_b], [model_a, model_b], x_val, y_val)
+
+###################
+# Test models
+##
+# First baseline: Just choose the first model everytime
+print("Just using model 1: {}".format(smape(y_test.squeeze().numpy(), model_a.predict(x_test))))
+# Second baseline: Just choose the second model everytime
+print("Just using model 2: {}".format(smape(y_test.squeeze().numpy(), model_b.predict(x_test))))
+# Third baseline: Just predict the last value as the next
+print("x+1 = x: {}".format(smape(y_test.squeeze().numpy(), x_test[..., -1].squeeze().numpy())))
+##
+# Try switching classifiers based on ROCS
+print("Using online methods: {}".format(smape(y_test.squeeze().numpy(), online_forecasting(val_rocs, [model_a, model_b], x_test))))
+###################
 
 # Visualize train results
-#compare_logs(logs_a, logs_b, smape_baseline=smape_baseline, mse_baseline=mse_bseline)
-
-# binary vector (one if model_a is better, zero otherwise)
-ranking = rank_models(preds_a, preds_b, x_val, y_val)
-gradcams = get_gradcam([model_a, model_b], [preds_a, preds_b], x_val, y_val)
-
+#compare_logs(logs_a, logs_b, smape_baseline=smape_baseline, mse_baseline=mse_baseline, mae_baseline=mae_baseline)
 # Example Visualizations
-#plot_cam(x_val[10:13], gradcams[0][10:13], title="Shallow_CNN_RNN")
-#plot_cam(x_val[10:13], gradcams[1][10:13], title="Shallow_FCN")
+#plot_cam(x_val[10:13], gradcams[0][10:13], title="Shallow_CNN_RNN", save_to="../plots/poc_gradcams_rnn.pdf")
+#plot_cam(x_val[10:13], gradcams[1][10:13], title="Shallow_FCN", save_to="../plots/poc_gradcams_cnn.pdf")
