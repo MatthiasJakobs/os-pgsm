7ef302f64b6f0669217756181046099049d94d84
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2021-11-24 10:23:21 +0100

    adapted NCL to new datasets

--- code/negative_correlation_learning.py
+++ code/negative_correlation_learning.py
@@ -5,14 +5,11 @@ import matplotlib.pyplot as plt
 import copy
 
 from typing import List
-from single_models import OneResidualFCN, Shallow_FCN
-from datasets.ucr_to_forecasting import AbnormalHeartbeat
-from datasets.utils import windowing, _apply_window, train_test_split
 from seedpy import fixedseed
-from experiments import single_models, skip_models_composit, implemented_datasets, m4_data_path
-from datasets import M4_Daily, M4_Hourly, M4_Monthly, M4_Quaterly, M4_Weekly, M4_Yearly
-from utils import calculate_single_seed, ncl_seed
+from experiments import single_models, implemented_datasets
+from utils import ncl_seed
 from os.path import exists
+from train_single_models import load_data
 
 class NegCorLearning(nn.Module):
 
@@ -102,23 +99,17 @@ class NegCorLearning(nn.Module):
         return best_val_loss, best_val_model
 
 def main():
-    # Test NCC
-
-    lag = 5
     repeats = 5
-    for ds_name, ds in implemented_datasets.items():
-        model_save_path = f"models/ncl/{ds_name}_lag{lag}.pth"
+    batch_size = 100
 
-        if exists(model_save_path):
-            print(f"[NCL] Skipping {ds_name} because it exists...")
+    for (ds_name, ds_index) in implemented_datasets:
+        save_path = f"models/{ds_name}/{ds_index}_ncl.pth"
+        if exists(save_path):
             continue
 
-        X = ds["ds"]().torch()
-        batch_size = ds["batch_size"]
+        X_train, y_train, X_val, y_val, X_test, y_test = load_data(ds_name, ds_index)
 
-        [x_train, x_val], [y_train, y_val], _, _, _ = windowing(X, train_input_width=lag, val_input_width=lag*lag, use_torch=True)
-
-        with fixedseed(torch, seed=ncl_seed(ds_name, lag)):
+        with fixedseed(torch, seed=ncl_seed(ds_name, ds_index)):
             losses = np.zeros((repeats))
             models = []
             for i in range(repeats):
@@ -127,17 +118,16 @@ def main():
                     m = model_obj["obj"]
                     nr_filters = model_obj["nr_filters"]
                     hidden_states = model_obj["hidden_states"]
-                    if m not in skip_models_composit:
-                            try:
-                                used_single_models.append(m(nr_filters=nr_filters, ts_length=x_train.shape[-1], hidden_states=hidden_states, batch_size=batch_size))
-                            except TypeError:
-                                used_single_models.append(m(nr_filters=nr_filters, ts_length=x_train.shape[-1], batch_size=batch_size))
+                    try:
+                        used_single_models.append(m(nr_filters=nr_filters, ts_length=X_train.shape[-1], hidden_states=hidden_states, batch_size=batch_size))
+                    except TypeError:
+                        used_single_models.append(m(nr_filters=nr_filters, ts_length=X_train.shape[-1], batch_size=batch_size))
 
                 model = NegCorLearning(used_single_models)
                 model.batch_size = batch_size
-                val_loss, best_model = model.fit(x_train, y_train, x_val, y_val, verbose=False)
+                val_loss, best_model = model.fit(X_train, y_train, X_val, y_val, verbose=False)
 
-                print(ds_name, i, val_loss)
+                print(ds_name, ds_index, i, val_loss)
 
                 losses[i] = val_loss
                 models.append(best_model)
@@ -146,67 +136,10 @@ def main():
             average_performance = np.mean(losses)
             rel_scores = np.abs(losses-average_performance)
             nearest_model_idx = np.argmin(rel_scores)
-            print(ds_name, "chose", nearest_model_idx)
+            print(ds_name, ds_index, "chose", nearest_model_idx)
             print("-"*30)
 
-            torch.save(models[nearest_model_idx], model_save_path)
-
-
-    name_list = ['hourly', 'weekly', 'quaterly', 'daily', 'monthly']
-    ds_list = [ M4_Hourly(path=m4_data_path), M4_Weekly(path=m4_data_path), M4_Quaterly(path=m4_data_path), M4_Daily(path=m4_data_path), M4_Monthly(path=m4_data_path)]
-
-    for name, ds in zip(name_list, ds_list):
-        indices = list(range(20))
-        for idx in indices:
-            padded_idx = ds.train_data.columns[idx]
-            model_save_path = f"models/ncl/m4_{name}_{padded_idx}_lag{lag}.pth"
-            if exists(model_save_path):
-                print(f"[NCL] Skipping m4_{name}_{padded_idx} because it exists...")
-                continue
-            ds_train, _ = ds.get(padded_idx)
-            ds_train, ds_val = train_test_split(ds_train, split_percentages=(2.0/3.0, 1.0/3.0))
-
-            if len(ds_train) <= lag or len(ds_val) <= lag:
-                print(padded_idx, "is to short, skipping")
-                continue
-
-            x_train, y_train = _apply_window(ds_train, lag)
-            x_val, y_val = _apply_window(ds_val, lag)
-
-            ds_name = f"m4_{name}_{padded_idx}"
-            batch_size = 500
-            with fixedseed(torch, seed=ncl_seed(ds_name, lag)):
-                losses = np.zeros((repeats))
-                models = []
-                for i in range(repeats):
-                    used_single_models = []
-                    for m_name, model_obj in single_models.items():
-                        m = model_obj["obj"]
-                        nr_filters = model_obj["nr_filters"]
-                        hidden_states = model_obj["hidden_states"]
-                        if m not in skip_models_composit:
-                                try:
-                                    used_single_models.append(m(nr_filters=nr_filters, ts_length=x_train.shape[-1], hidden_states=hidden_states, batch_size=batch_size))
-                                except TypeError:
-                                    used_single_models.append(m(nr_filters=nr_filters, ts_length=x_train.shape[-1], batch_size=batch_size))
-
-                    model = NegCorLearning(used_single_models)
-                    model.batch_size = batch_size
-                    val_loss, best_model = model.fit(x_train, y_train, x_val, y_val, verbose=False)
-
-                    print(ds_name, i, val_loss)
-
-                    losses[i] = val_loss
-                    models.append(best_model)
-
-                # Choose the most average model
-                average_performance = np.mean(losses)
-                rel_scores = np.abs(losses-average_performance)
-                nearest_model_idx = np.argmin(rel_scores)
-                print(ds_name, "chose", nearest_model_idx)
-                print("-"*30)
-
-                torch.save(models[nearest_model_idx], model_save_path)
+            torch.save(models[nearest_model_idx], save_path)
 
 if __name__ == "__main__":
     main()
