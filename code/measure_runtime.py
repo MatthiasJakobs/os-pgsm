d80cae73454cf57d54a30f1f5b4278c164b0e2bc
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2021-12-08 22:09:01 +0100

    final version of runtime test

--- code/measure_runtime.py
+++ code/measure_runtime.py
@@ -13,29 +13,23 @@ from warnings import simplefilter
 simplefilter(action="ignore", category=UserWarning)
 used_datasets = [
     ("electricity_hourly", 0),
+    ("AbnormalHeartbeat", 0),
     ("Phoneme", 0),
+    ("Rock", 0),
     ("kdd_cup_2018", 0),
 ]
 
 used_experiments = [
-    # (OS_PGSM, min_distance_drifts(name="OEP-ROC-5", nr_clusters_ensemble=5)),
-    # (OS_PGSM, min_distance_drifts(name="OEP-ROC-15")),
-    # (OS_PGSM, min_distance_drifts(name="OEP-ROC-10", nr_clusters_ensemble=10)),
-    # (OS_PGSM, min_distance_drifts(name="OEP-ROC-20", nr_clusters_ensemble=20)),
-    # (OS_PGSM, min_distance_drifts(name="OEP-ROC-C", skip_topm=True)),
-    (OS_PGSM, min_distance_drifts(name="OEP-ROC-TOP", skip_clustering=True)),
-    # (OS_PGSM, min_distance_drifts(name="OEP-ROC-II", skip_type1=True)),
-    # (OS_PGSM, min_distance_drifts(name="OEP-ROC-I", skip_type2=True)),
+    (OS_PGSM, min_distance_drifts(name="OEP-ROC-15")),
     (OS_PGSM, min_distance_drifts(name="OEP-ROC-ST", skip_drift_detection=True)),
-    # (OS_PGSM, min_distance_drifts(name="OEP-ROC-Per", concept_drift_detection="periodic")),
-    # (OS_PGSM, min_distance_drifts(name="OEP-ROC-15-topm-10", nr_select=10, skip_topm=True)),
+    (OS_PGSM, min_distance_drifts(name="OEP-ROC-Per", concept_drift_detection="periodic")),
 ]
 
 def measure_runtime():
     runtime_dict = dict()
     for ds_name, ds_index in used_datasets:
         X = torch.from_numpy(load_dataset(ds_name, ds_index)).float()
-        models, model_names = load_models(ds_name, ds_index, return_names=True)
+        models, _ = load_models(ds_name, ds_index, return_names=True)
 
         [_, _], [_, _], _, X_val, X_test = windowing(X, train_input_width=5, val_input_width=25, use_torch=True)
 
